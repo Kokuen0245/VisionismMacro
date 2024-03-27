ab58daa6ca66e0c3b67814fdbf1bb7c487dc5856
Kokuen0245 <noreply@example.com>
2024-03-27 15:01:19 -0400

    Hopefully improved get on training again again?

--- main.py
+++ main.py
@@ -86,11 +86,7 @@ def click_start_button():
 
 def get_on_training():
     try:
-        image = cv2.imread(hold_e_button_path)
-
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        hold_e_button_location = pyautogui.locateCenterOnScreen(gray_image, grayscale=True, confidence=0.8)
+        hold_e_button_location = pyautogui.locateCenterOnScreen(hold_e_button_path, confidence=0.8)
         if hold_e_button_location:
             time.sleep(2)
             hold_key("e", 2)
